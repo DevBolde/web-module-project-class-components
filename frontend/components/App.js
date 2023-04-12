0149bdb08ab436b49120cd00d4229971ea96917a
Devin Bolden <devinbolden6@gmail.com>
2023-04-12 17:23:15 -0500

    builds from state and complete flags

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -4,18 +4,42 @@ export default class App extends React.Component {
   constructor(){
     super();
     this.state ={
-      list: ''
+      todos: [
+          {
+            name: 'Organize Garage',
+            id: 1528817077286, // could look different, you could use a timestamp to generate it
+            completed: false
+          },
+          {
+            name: 'Bake Cookies',
+            id: 1528817084358,
+            completed: false
+          },
+          {
+            name: 'Monkey around',
+            id: 1528817084359,
+            completed: false,
+          },
+          {
+            name: 'Build Project',
+            id: 1528817084360,
+            completed: false
+          }
+        ]
     }
 
   }
   render() {
+    const { todos } = this.state;
     return (
       <div>
         <h1>Todos</h1>
         <ul>
-        <li>Walk The Dog</li>
-        <li>Take out the Trash</li>
-        <li>Workout</li>
+          {
+            todos.map(ele => {
+              return (<li>{ele.name} { todos.completed?<span>-completed-</span>:<span></span> }</li>)
+            })
+          }
         </ul>
 
         <form>
