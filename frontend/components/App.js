20a7160b049107ce1605bf478ac3372f5f464eda
Devin Bolden <devinbolden6@gmail.com>
2023-04-12 16:32:16 -0500

    Added in all dom

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,10 +1,29 @@
 import React from 'react'
 
 export default class App extends React.Component {
+  constructor(){
+    super();
+    this.state ={
+      list: ''
+    }
+
+  }
   render() {
     return (
       <div>
-        Todo App
+        <h1>Todos</h1>
+        <ul>
+        <li>Walk The Dog</li>
+        <li>Take out the Trash</li>
+        <li>Workout</li>
+        </ul>
+
+        <form>
+          <input />
+          <button>Add</button>
+        </form>
+
+        <button>Clear</button>
       </div>
     )
   }
